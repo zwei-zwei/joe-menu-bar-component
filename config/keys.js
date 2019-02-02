f13cc5d899322741f220035ee8c703f49e261204
Joe Boulos <jab12288@gmail.com>
2019-02-02 14:53:02 -0800

    updated mongo atlas connection string to free cluster

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://joe123:<PASSWORD>@sdc-shard-00-00-xcsds.mongodb.net:27017,sdc-shard-00-01-xcsds.mongodb.net:27017,sdc-shard-00-02-xcsds.mongodb.net:27017/test?ssl=true&replicaSet=sdc-shard-0&authSource=admin&retryWrites=true',
+  mongoURI: 'mongodb://joe123:123@sdc-shard-00-00-ihwpn.mongodb.net:27017,sdc-shard-00-01-ihwpn.mongodb.net:27017,sdc-shard-00-02-ihwpn.mongodb.net:27017/test?ssl=true&replicaSet=sdc-shard-0&authSource=admin&retryWrites=true',
 };
 
