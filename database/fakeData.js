900612aa31339072a71180f8578fb5a4f4187553
Joe Boulos <jab12288@gmail.com>
2019-02-03 16:52:47 -0800

    successfully loaded 10m docs to mongodb ~ 15mins

--- database/fakeData.js
+++ database/fakeData.js
@@ -1,187 +1,35 @@
-// const faker = require('faker');
-// const mysql = require('mysql');
-
-
-// let userDataGenerator = function () {
-//   let userData = [];
-
-//   for (let i = 0; i < 100; i++) {
-//     let user = faker.fake("{{internet.userName}}, {{image.avatar}}, {{image.image}}, {{commerce.department}}, {{random.number}}, {{random.number}}");
-//     user.trim();
-//     userData.push(user.split(','));
-//   }
-
-//   let results = userData.map(arr => {
-//     let obj = {};
-//     for (let i = 0; i < arr.length; i++) {
-//       if (i === 0) obj['display_name'] = arr[0];
-//       if (i === 1) obj['logo'] = arr[1];
-//       if (i === 2) obj['profile_image'] = arr[2];
-//       if (i === 3) obj['category'] = arr[3];
-//       if (i === 4) obj['followers'] = Number(arr[4]);
-//       if (i === 5) obj['followings'] = Number(arr[5]);
-//     }
-//     return obj
-//   });
-
-//   return results;
-// };
-
-// let followersGenerator = function () {
-//   let userData = [];
-
-//   for (let i = 0; i < 10; i++) {
-//     let user = faker.fake("{{internet.userName}}, {{image.avatar}}, {{commerce.department}}");
-//     user.trim();
-//     userData.push(user.split(','));
-//   }
-
-//   let results = userData.map(arr => {
-//     let obj = {};
-//     for (let i = 0; i < arr.length; i++) {
-//       if (i === 0) obj['display_name'] = arr[0];
-//       if (i === 1) obj['logo'] = arr[1];
-//       if (i === 2) obj['category'] = arr[2];
-//     }
-//     return obj
-//   });
-
-//   return results;
-// };
-
-
-
-// const connection = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'milenita1809!',
-//   database: 'users'
-// });
-
-
-// const insertDataToDatabase = function () {
-//   for (var i = 0; i < 100; i++) {
-//     let values = userDataGenerator();
-//     let sql = `INSERT INTO users (display_name, logo, profile_image_url, category, followers, following) VALUES ('${values[i].display_name}', '${values[i].logo}', '${values[i].profile_image}', '${values[i].category}', ${values[i].followers}, ${values[i].followings}) `;
-//     connection.query(sql, function (err) {
-//       if (err) {
-//         console.log(err);
-//       }
-//     });
-//   }
-//   console.log('Records inserted');
-// }
-
-
-// const followersToDatabase = function () {
-//   for (var i = 0; i < 10; i++) {
-//     let values = followersGenerator();
-//     let sql = `INSERT INTO followers (display_name, logo, category) VALUES ('${values[i].display_name}', '${values[i].logo}', '${values[i].category}') `;
-//     connection.query(sql, function (err) {
-//       if (err) {
-//         console.log(err);
-//       }
-//     });
-//   }
-//   console.log('Records inserted');
-// }
-
-
-// connection.connect(function (err) {
-//   if (err) {
-//     return console.error('error ' + err.message);
-//   }
-
-//   console.log('Connected to the MySQL server. ');
-//   insertDataToDatabase();
-//   followersToDatabase();
-// });
-
-
-// const faker = require('faker');
-// // const mysql = require('mysql');
-// const mongoose = require('mongoose');
-// const onlineDb = require('../config/keys');
-// mongoose.connect(onlineDb.mongoURI, { useNewUrlParser: true });
-
-// const db = mongoose.connection;
-// mongoose.Promise = global.Promise;
-
-
-// let userDataGenerator = function () {
-//   let userData = [];
-
-//   for (let i = 0; i < 100; i++) {
-//     let user = faker.fake("{{internet.userName}}, {{image.avatar}}, {{image.image}}, {{commerce.department}}, {{random.number}}, {{random.number}}");
-//     user.trim();
-//     userData.push(user.split(','));
-//   }
-
-//   let results = userData.map(arr => {
-//     let obj = {};
-//     for (let i = 0; i < arr.length; i++) {
-//       if (i === 0) obj['display_name'] = arr[0];
-//       if (i === 1) obj['logo'] = arr[1];
-//       if (i === 2) obj['profile_image'] = arr[2];
-//       if (i === 3) obj['category'] = arr[3];
-//       if (i === 4) obj['followers'] = Number(arr[4]);
-//       if (i === 5) obj['followings'] = Number(arr[5]);
-//     }
-//     return obj
-//   });
-
-//   return results;
-// };
-
-
-
-// const insertDataToDatabase = function () {
-//   for (var i = 0; i < 100; i++) {
-//     let values = userDataGenerator();
-//     let sql = `INSERT INTO users (display_name, logo, profile_image_url, category, followers, following) VALUES ('${values[i].display_name}', '${values[i].logo}', '${values[i].profile_image}', '${values[i].category}', ${values[i].followers}, ${values[i].followings}) `;
-//     connection.query(sql, function (err) {
-//       if (err) {
-//         console.log(err);
-//       }
-//     });
-//   }
-//   console.log('Records inserted');
-// }
-
-
-
-// connection.connect(function (err) {
-//   if (err) {
-//     return console.error('error ' + err.message);
-//   }
-
-//   console.log('Connected to the MySQL server. ');
-//   insertDataToDatabase();
-//   followersToDatabase();
-// });
-
 const UserDb = require('../database/Users');
 const faker = require('faker');
 
+// set this to en_US so faker generated data is in english
 faker.locale = 'en_US';
 
-//require('events').EventEmitter.prototype._maxListeners = 1000;
+// stack overflow recommendation, node property
+require('events').EventEmitter.prototype._maxListeners = 1000;
+let count = 1;
 
-const seedUser = () => {
-  for(let i = 0; i < 10000; i++) {
-    // create may not work, may need to use insertMany method
-    // for larger numbers
-    UserDb.create({
-      user_id: faker.random.number(),
-      display_name: faker.internet.userName(),
-      logo: faker.image.imageUrl(),
-      profile_image_url: faker.internet.url(),
-      category: faker.company.bsAdjective(),
-      followers: faker.random.number(),
-      Following: faker.random.number(),
-    })
-    .catch(err => console.log(err));
+// use async and await to load large numbers of data into db
+// use 'insertMany()' method instead of 'create' for faster load times
+
+async function seedUser(outerLoop, innerLoop) {
+  for(let i = 0; i < outerLoop; i++) {
+    // need to insert an array of objects into the db
+    let arr = [];
+    for(let j = 0; j < innerLoop; j++) {
+      let userObject = {
+        user_id: faker.random.number(),
+        display_name: faker.internet.userName(),
+        logo: faker.image.imageUrl(),
+        profile_image_url: faker.internet.url(),
+        category: faker.company.bsAdjective(),
+        followers: faker.random.number(),
+        Following: faker.random.number(),
+      }
+      arr.push(userObject);
+    }
+    console.log('Seed successful', count++);
+    await UserDb.insertMany(arr);
   }
 }
 
-seedUser();
+seedUser(1000, 10000);
